f3f83021dffbd67a8e26eb4e7cf1aeab61fb05d3
es2ice <es2ice@hot.ee>
2022-08-26 13:19:01 +0300

    next_upload = time.monotonic()

--- auto_rx/autorx/aprs.py
+++ auto_rx/autorx/aprs.py
@@ -752,10 +752,10 @@ class APRSUploader(object):
         if self.input_processing_running:
             if float(telemetry['vel_v']) < -1 and float(telemetry['alt']) < 1000:
                 self.upload_time = 5
-                self.next_upload = time.monotonic() + self.upload_time
+                self.next_upload = time.monotonic()
             elif float(telemetry['vel_v']) < -1 and float(telemetry['alt']) < 1500:
                 self.upload_time = 10
-                self.next_upload = time.monotonic() + self.upload_time
+                self.next_upload = time.monotonic()
             elif float(telemetry['alt']) > 10000:
                 self.upload_time = 60
             else:
